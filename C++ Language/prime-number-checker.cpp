cd3310eeac7c6a5f271340214149969594e43c83
Samar <samarbabar02@gmail.com>
2025-04-07 16:54:10 +0500

    Update

---
+++ C++ Language/prime-number-checker.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 0;
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    if (num <= 1) {
+            cout << num << " is not a prime number." << endl;
+        } 
+    else {
+            for (int i = 1; i <= num; i++)
+             {
+                if (num % i == 0) 
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2) 
+             {
+                cout << num << " is a prime number." << endl;
+            } 
+            else{
+                cout << num << " is not a prime number." << endl;
+                           }                                                                                                        }
+return 0;
+  }                
+                                                                                                                                            
---
+++ C++ Language/print-pattern.cpp
@@ -0,0 +1,21 @@
+//A program to generate the following pattern using a nested for loop:
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+#include <iostream>
+using namespace std;
+int main() 
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1; j<=i; j++ )
+            {
+                    cout<<"*";
+                        }
+        cout<<endl;
+    }
+
+    return 0;
+}
